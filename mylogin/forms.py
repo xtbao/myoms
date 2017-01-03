f6fe771e85b29bd7c6c32277228faa661c241494
root <root@xtbip.com>
2017-01-03 17:41:31 +0800

    forms zhu shi

--- mylogin/forms.py
+++ mylogin/forms.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 from django import forms
 from django.contrib.auth.models import User
-#from bootstrap_toolkit.widgets import BootstrapDateInput,BootstrapTextInput,BootstrapUneditableInput
 
 class LoginForm(forms.Form):
     username = forms.CharField(
